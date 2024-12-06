c4db0d04ab3d7a5738906e0938f31279cb39721a
Robert Nantchouang Nantchouang <robertnantchouangn@gmail.com>
2024-12-06 18:11:56 +0100

    Update codeforce689b.py

--- codeforce689b.py
+++ codeforce689b.py
@@ -1,39 +1,41 @@
 from collections import deque
 from collections import defaultdict
 
-def mikeAndShortcuts(n, shortcuts):
-    # Zero base shortcuts for simplicity.
-    shortcuts = [x - 1 for x in shortcuts]
+n = int(input())
+shortcuts = list(map(int, input().split()))
+
+# Zero base shortcuts for simplicity.
+shortcuts = [x - 1 for x in shortcuts]
+
+# Create graph to represent intersections.
+adjList = defaultdict(set)
     
-    # Create graph to represent intersections.
-    adjList = defaultdict(set)
-        
-    # Link each adjacent intersections
-    for node in range(1, n):
-        prevNode = node - 1
-        adjList[prevNode].add(node)
-    
-    # Add shortcuts
-    for (node, nearestNode) in enumerate(shortcuts):
-        if node != nearestNode:
-            adjList[node].add(nearestNode)
+# Link each adjacent intersections
+for node in range(1, n):
+    prevNode = node - 1
+    adjList[prevNode].add(node)
 
-    # Perform bfs to get shortest time since graph has one-unit edges.
-    visited = [False] * n
-    startNode = 0
-    q = deque()
-    q.append((startNode, 0))
+# Add shortcuts
+for (node, nearestNode) in enumerate(shortcuts):
+    if node != nearestNode:
+        adjList[node].add(nearestNode)
 
-    time = 0
-    timePerNode = [0] * n
-    while len(q) != 0:
-        node, time = q.pop()
-        visited[node] = True
-        timePerNode[node] = time
+# Perform bfs to get shortest time since graph has one-unit edges.
+visited = [False] * n
+startNode = 0
+q = deque()
+q.append((startNode, 0))
 
-        for child in adjList[node]:
-            if visited[child] == False:
-                q.append((child, time + 1))
+time = 0
+timePerNode = [0] * n
+while len(q) != 0:
+    node, time = q.pop()
+    visited[node] = True
+    timePerNode[node] = time
 
-    for time in timePerNode:
-        print(time, end="")
+    for child in adjList[node]:
+        if visited[child] == False:
+            q.append((child, time + 1))
+
+for time in timePerNode:
+    print(time, end=" ")
